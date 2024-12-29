9d1024ba3aae3d7d79ce21139b1aa38484fa39e0
abhishek-Roy22 <100830257+abhishek-Roy22@users.noreply.github.com>
2024-12-29 17:55:14 +0530

    Created Signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,13 +1,77 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 
 const Signup = () => {
+  const [error, setError] = useState({});
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirm_password: '',
+  });
+
+  function formValidation() {
+    const newError = {};
+
+    if (!formData.username) {
+      newError.username = 'Username is required';
+    }
+
+    if (!formData.email) {
+      newError.email = 'Email is required';
+    }
+
+    if (!formData.password) {
+      newError.password = 'Password is required';
+    }
+
+    if (!formData.confirm_password) {
+      newError.confirm_password = 'Confirm password is required';
+    } else if (formData.password !== formData.confirm_password) {
+      newError.confirm_password = 'Password is not match';
+    }
+
+    setError(newError);
+    return Object.keys(newError).length === 0;
+  }
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+
+    // clear the error when user start typing
+    if (error[name]) {
+      setError((prev) => ({
+        ...prev,
+        [name]: '',
+      }));
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!formValidation()) {
+      toast.error('Add required inputs', {
+        position: 'top-center',
+      });
+      return;
+    }
+
+    toast.success('Form submitted successful');
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
       <div className="w-full max-w-md p-8 space-y-6 bg-white dark:bg-gray-800 rounded shadow-md">
         <h2 className="text-2xl font-bold text-center text-gray-900 dark:text-gray-100">
           Register
         </h2>
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={handleSubmit}>
           <div>
             <label
               htmlFor="username"
@@ -16,13 +80,17 @@ const Signup = () => {
               Username
             </label>
             <input
+              value={formData.username}
+              onChange={handleChange}
               id="username"
               name="username"
               type="text"
-              required
               placeholder="Enter your username"
               className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
             />
+            {error.username && (
+              <p className="text-xs text-rose-800">{error.username}</p>
+            )}
           </div>
           <div>
             <label
@@ -32,14 +100,18 @@ const Signup = () => {
               Email address
             </label>
             <input
+              value={formData.email}
+              onChange={handleChange}
               id="email"
               name="email"
               type="email"
               autoComplete="email"
-              required
               placeholder="Enter your email"
               className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
             />
+            {error.email && (
+              <p className="text-xs text-rose-800">{error.email}</p>
+            )}
           </div>
           <div>
             <label
@@ -49,14 +121,39 @@ const Signup = () => {
               Password
             </label>
             <input
+              value={formData.password}
+              onChange={handleChange}
               id="password"
               name="password"
               type="password"
               autoComplete="current-password"
-              required
               placeholder="Enter your password"
               className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
             />
+            {error.password && (
+              <p className="text-xs text-rose-800">{error.password}</p>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="confirm_password"
+              className="block text-sm font-medium text-gray-700 dark:text-gray-300"
+            >
+              Confirm Password
+            </label>
+            <input
+              value={formData.confirm_password}
+              onChange={handleChange}
+              id="confirm_password"
+              name="confirm_password"
+              type="password"
+              autoComplete="current-password"
+              placeholder="Enter your password again"
+              className="w-full px-3 py-2 mt-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+            />
+            {error.confirm_password && (
+              <p className="text-xs text-rose-800">{error.confirm_password}</p>
+            )}
           </div>
           <div>
             <button
@@ -76,6 +173,7 @@ const Signup = () => {
           </div>
         </form>
       </div>
+      <ToastContainer theme="dark" />
     </div>
   );
 };
